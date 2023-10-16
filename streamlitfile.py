c7057582ac4ed3cf97188d26e18cacff367f574b
Anas Mokhtari <89369540+Dej97@users.noreply.github.com>
2023-10-16 21:55:31 +0100

    Update streamlitfile.py

--- streamlitfile.py
+++ streamlitfile.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd 
-import matplotlib.pyplot as plt
+
 import plotly.express as px 
-import base64
+
 from sklearn.preprocessing import LabelEncoder
 import seaborn as sbr
 
