9d9a4d7674e40603ed904b511cbc23e1af83c009
Anas Mokhtari <89369540+Dej97@users.noreply.github.com>
2023-10-16 22:00:00 +0100

    Add files via upload

--- streamlitfile.py
+++ streamlitfile.py
@@ -1,10 +1,6 @@
-pip install plotly plotly-express
-
 import streamlit as st
 import pandas as pd 
-
 import plotly.express as px 
-
 from sklearn.preprocessing import LabelEncoder
 import seaborn as sbr
 
